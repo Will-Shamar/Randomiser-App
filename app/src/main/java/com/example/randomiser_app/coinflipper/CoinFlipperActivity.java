d0acdd13e1d38424f274d638eb26ef3c46285e62
Shamar <Shamar098@gmail.com>
2025-02-21 22:58:13 +0000

    feat: add custom toolbar to coinflipper

--- app/src/main/java/com/example/randomiser_app/coinflipper/CoinFlipperActivity.java
+++ app/src/main/java/com/example/randomiser_app/coinflipper/CoinFlipperActivity.java
@@ -2,24 +2,43 @@ package com.example.randomiser_app.coinflipper;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 
 import com.example.randomiser_app.R;
+import com.example.randomiser_app.databinding.ActivityCoinFlipperBinding;
+import com.example.randomiser_app.databinding.ActivityMainBinding;
 
 public class CoinFlipperActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_coin_flipper);
-        Toolbar myToolbar = findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
-        getSupportActionBar().setTitle("Coin Flipper");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+//        setContentView(R.layout.activity_coin_flipper);
+//        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+//        setSupportActionBar(myToolbar);
+//        getSupportActionBar().setTitle("Coin Flipper");
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        ActivityCoinFlipperBinding binding = ActivityCoinFlipperBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Toolbar toolbar =  findViewById(R.id.MAINTOOL);
+        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        TextView whatIneed = findViewById(R.id.maintoolbartitle);
+        whatIneed.setText("COIN FLIPPER");
+
+
+
+
+
+
+
     }
 
 
