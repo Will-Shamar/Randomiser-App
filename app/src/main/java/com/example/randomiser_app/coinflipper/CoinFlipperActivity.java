7277f33a06bd310292fe7723e9ace6a43cc8a12f
Shamar <Shamar098@gmail.com>
2025-02-22 22:14:02 +0000

    feat: add custom toolbar to CoinFlipperActivity

--- app/src/main/java/com/example/randomiser_app/coinflipper/CoinFlipperActivity.java
+++ app/src/main/java/com/example/randomiser_app/coinflipper/CoinFlipperActivity.java
@@ -1,6 +1,9 @@
 package com.example.randomiser_app.coinflipper;
 
+import static android.view.View.TEXT_ALIGNMENT_TEXT_START;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -18,22 +21,26 @@ public class CoinFlipperActivity extends AppCompatActivity {
         // Need to set bindings manually and get the view from that in order to databind
         ActivityCoinFlipperBinding binding = ActivityCoinFlipperBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        Toolbar toolbar =  findViewById(R.id.MAINTOOL);
-        setSupportActionBar(toolbar);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        TextView whatIneed = findViewById(R.id.maintoolbartitle);
-        whatIneed.setText("COIN FLIPPER");
-
-
-
-
-
-
-
+        setUpToolbar();
     }
 
+    private void setUpToolbar(){
+
+        Toolbar toolbar =  findViewById(R.id.customtoolbar);
+        Log.i("CoinFlipperActivity", "toolbar: " + toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setToolbarText();
+    }
+
+    private void setToolbarText(){
+        TextView toolbarTitle = findViewById(R.id.maintoolbartitle);
+        toolbarTitle.setText(R.string.coin_flippers_toolbar_title);
+        toolbarTitle.setTextAlignment(TEXT_ALIGNMENT_TEXT_START);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
