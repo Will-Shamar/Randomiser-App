db834687ded1a6bdc2b041d901725582f08b6ad1
Shamar <Shamar098@gmail.com>
2025-02-26 17:29:32 +0000

    feat: add toolbar set up and logic in DeckShufflerActivity

--- app/src/main/java/com/example/randomiser_app/ui/deckshuffler/DeckShufflerActivity.java
+++ app/src/main/java/com/example/randomiser_app/ui/deckshuffler/DeckShufflerActivity.java
@@ -1,17 +1,54 @@
 package com.example.randomiser_app.ui.deckshuffler;
 
+import static android.view.View.TEXT_ALIGNMENT_TEXT_START;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.randomiser_app.R;
+import com.example.randomiser_app.databinding.ActivityDeckShufflerBinding;
 
 public class DeckShufflerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_deck_shuffler);
 
+        ActivityDeckShufflerBinding binding = ActivityDeckShufflerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setUpToolbar();
     }
+
+    private void setUpToolbar(){
+
+        Toolbar toolbar =  findViewById(R.id.customtoolbar);
+        Log.i("DeckShufflerActivity", "toolbar: " + toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setToolbarText();
+    }
+
+    private void setToolbarText(){
+        TextView toolbarTitle = findViewById(R.id.maintoolbartitle);
+        toolbarTitle.setText(R.string.deck_shuffler_title);
+        toolbarTitle.setTextAlignment(TEXT_ALIGNMENT_TEXT_START);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
