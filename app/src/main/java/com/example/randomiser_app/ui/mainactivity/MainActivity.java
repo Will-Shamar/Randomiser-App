c389e950ae4316589389e993aa88df37224f9b1d
WI91 <elammar.will@gmail.com>
2025-02-26 16:17:46 +0000

    Refactor: Correct game names in onItemClick method

--- app/src/main/java/com/example/randomiser_app/ui/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/randomiser_app/ui/mainactivity/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.randomiser_app.R;
+import com.example.randomiser_app.coinflipper.CoinFlipperActivity;
 import com.example.randomiser_app.databinding.ActivityMainBinding;
 import com.example.randomiser_app.model.Game;
 import com.example.randomiser_app.ui.adapter.GameAdapter;
@@ -49,29 +50,29 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
     @Override
     public void onItemClick(int position) {
 
-//        Intent intent= null;
-//        switch (gameList.get(position).getName()) {
+        Intent intent = new Intent();
+        switch (gameList.get(position).getName()) {
 //
-//            case "diceRoll":
+//            case "Dice Roll":
 //                intent = new Intent(MainActivity.this, DiceRollerActivity.class);
 //                break;
-//            case "coinFlip":
-//                intent = new Intent(MainActivity.this, CoinFlipperActivity.class);
-//                break;
-//            case "cardPick":
+            case "Coin Flip":
+                intent = new Intent(MainActivity.this, CoinFlipperActivity.class);
+                break;
+//            case "Pick a Card":
 //                intent = new Intent(MainActivity.this, CardPickerActivity.class);
 //                break;
-//            case "drawStraws":
+//            case "Draw Straws":
 //                intent = new Intent(MainActivity.this, DrawStrawsActivity.class);
 //                break;
 //
-//            default:
-//                Toast.makeText(this, "Unable to launch game", Toast.LENGTH_SHORT)
-//                        .show();
-//
-//        }
-//
-//        startActivity(intent);
+            default:
+                Toast.makeText(this, "Unable to launch game", Toast.LENGTH_SHORT)
+                        .show();
+
+        }
+
+        startActivity(intent);
 //
 //
 //        // TODO: 20/02/2025 Add logic for launching separate game activities
