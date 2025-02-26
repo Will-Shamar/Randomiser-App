97bf48c9d0bdae0f47015ae6dddd66527dbbf5db
WI91 <elammar.will@gmail.com>
2025-02-26 14:45:30 +0000

    Feat: Add new fields for theme and fontStyle to game model

--- app/src/main/java/com/example/randomiser_app/model/Game.java
+++ app/src/main/java/com/example/randomiser_app/model/Game.java
@@ -1,13 +1,24 @@
 package com.example.randomiser_app.model;
 
+import android.graphics.Typeface;
+import android.graphics.fonts.FontStyle;
+
+import com.example.randomiser_app.R;
+
 public class Game {
 
     String name;
     int image;
+    int theme;
 
-    public Game(String name, int image) {
+    int fontStyle;
+
+    public Game(String name, int image, int theme, int fontStyle) {
         this.name = name;
         this.image = image;
+        this.theme = theme;
+        this.fontStyle = fontStyle;
+
     }
 
     public String getName() {
@@ -17,4 +28,17 @@ public class Game {
     public int getImage() {
         return image;
     }
-}
+
+    public int getTheme() {
+        return theme;
+    }
+
+    public int getFontStyle() {
+        return fontStyle;
+    }
+
+    }
+
+
+
+
