449e9fa7c2accd753ae72f6b47207d583349b808
WI91 <elammar.will@gmail.com>
2025-02-26 14:47:18 +0000

    Feat: Add colour theme and fontStyle to game objects in repository class

--- app/src/main/java/com/example/randomiser_app/repository/GameRepository.java
+++ app/src/main/java/com/example/randomiser_app/repository/GameRepository.java
@@ -1,5 +1,8 @@
 package com.example.randomiser_app.repository;
 
+import android.graphics.fonts.Font;
+import android.graphics.fonts.FontStyle;
+
 import com.example.randomiser_app.R;
 import com.example.randomiser_app.model.Game;
 
@@ -10,10 +13,10 @@ public class GameRepository {
 
     public List<Game> getAllGames(){
 
-        Game diceRoll = new Game("Dice Roll", R.drawable.ic_launcher_foreground );
-        Game coinFlip = new Game("Coin Flip", R.drawable.ic_launcher_foreground );
-        Game cardPick = new Game("Pick a Card", R.drawable.ic_launcher_foreground  );
-        Game drawStraws = new Game("Draw Straws", R.drawable.ic_launcher_foreground );
+        Game diceRoll = new Game("Dice Roll", R.drawable.ic_launcher_foreground, R.drawable.gradient_colour_2, R.font.caveat_variablefont_wght);
+        Game coinFlip = new Game("Coin Flip", R.drawable.ic_launcher_foreground, R.drawable.gradient_colour_4, R.font.dancingscript_variablefont_wght);
+        Game cardPick = new Game("Pick a Card", R.drawable.ic_launcher_foreground, R.drawable.gradient_colour_1, R.font.indieflower_regular);
+        Game drawStraws = new Game("Draw Straws", R.drawable.ic_launcher_foreground, R.drawable.gradient_colour_3, R.font.moondance_regular);
 
         // TODO: 18/02/2025 Add images to drawable folder & reference above 
 
