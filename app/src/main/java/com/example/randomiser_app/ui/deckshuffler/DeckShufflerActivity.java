4b8a473afb83bae3d651262d09840d9167def012
Shamar <Shamar098@gmail.com>
2025-02-26 17:01:05 +0000

    feat: add deck shuffler activity and layout

---
+++ app/src/main/java/com/example/randomiser_app/ui/deckshuffler/DeckShufflerActivity.java
@@ -0,0 +1,17 @@
+package com.example.randomiser_app.ui.deckshuffler;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.randomiser_app.R;
+
+public class DeckShufflerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deck_shuffler);
+
+    }
+}
