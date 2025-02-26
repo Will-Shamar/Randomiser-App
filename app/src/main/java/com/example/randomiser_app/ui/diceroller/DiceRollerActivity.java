64af9079f0978ab4c37eb6a1e7965f0b1d59462c
Shamar <Shamar098@gmail.com>
2025-02-26 14:32:06 +0000

    feat: add dice roller activity and layout

---
+++ app/src/main/java/com/example/randomiser_app/ui/diceroller/DiceRollerActivity.java
@@ -0,0 +1,18 @@
+package com.example.randomiser_app.ui.diceroller;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.example.randomiser_app.R;
+
+public class DiceRollerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dice_roller);
+
+    }
+}
