4d259ed26a6e38b8dc8ba712192095a5c3569205
WI91 <elammar.will@gmail.com>
2025-03-04 16:57:37 +0000

    Feat: Add animation test to clickhandler class

--- app/src/main/java/com/example/randomiser_app/coinflipper/CoinFlipperClickHandler.java
+++ app/src/main/java/com/example/randomiser_app/coinflipper/CoinFlipperClickHandler.java
@@ -1,4 +1,32 @@
 package com.example.randomiser_app.coinflipper;
 
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.randomiser_app.R;
+
 public class CoinFlipperClickHandler {
+
+    private Context context;
+
+    public CoinFlipperClickHandler(Context context) {
+        this.context = context;
+    }
+
+    private void onFlipCoinButtonClicked () {
+
+        CoinFlipperActivity coinFlipperActivity = (CoinFlipperActivity) context;
+        ImageView coinImage = coinFlipperActivity.findViewById(R.id.coinimage);
+        coinImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                coinImage.animate()
+                        .setDuration(1000)
+                        .rotationXBy(360f)
+                        .start();
+
+            }
+        });
+    }
 }
