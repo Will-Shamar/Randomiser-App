76d58d6b4e835a944ce8f0c3c4d5b9a01bec749a
Shamar <Shamar098@gmail.com>
2025-04-11 23:52:44 +0100

    feat: style/change alertdialog to dialog

--- app/src/main/java/com/example/randomiser_app/ui/InfoButtonFragment.java
+++ app/src/main/java/com/example/randomiser_app/ui/InfoButtonFragment.java
@@ -1,10 +1,8 @@
 package com.example.randomiser_app.ui;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,6 +11,8 @@ import androidx.fragment.app.DialogFragment;
 
 import com.example.randomiser_app.R;
 
+import java.util.Objects;
+
 
 public class InfoButtonFragment extends DialogFragment {
     private final String TAG = "MAINMENUFRAG";
@@ -20,14 +20,12 @@ public class InfoButtonFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
-        LayoutInflater inflater = getLayoutInflater();
-        View view = inflater.inflate(R.layout.fragment_info_button,null);
-        builder.setView(view);
-
-        setupText(view);
-        return builder.create();
-
+        Dialog dialog = new Dialog(this.requireContext());
+        dialog.setContentView(R.layout.fragment_info_button);
+        setupText(page,dialog);
+        Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawableResource(R.drawable.info_button_box_style);
+        dialog.show();
+        return dialog;
     }
 
     private void setupText(View view){
