59fba9fcebdde767d9a5c5891478eb9213df6766
Shamar <Shamar098@gmail.com>
2025-04-15 17:23:20 +0100

    feat: implement info button text for coin flipper

--- app/src/main/java/com/example/randomiser_app/ui/infobutton/InfoButtonFragment.java
+++ app/src/main/java/com/example/randomiser_app/ui/infobutton/InfoButtonFragment.java
@@ -57,7 +57,29 @@ public class InfoButtonFragment extends DialogFragment {
                 ((TextView)dialog.findViewById(R.id.infoContent)).setText(String.format("%s%s%s%s", getString(R.string.info_button_main_content_p1), getString(R.string.info_button_main_content_p2), getString(R.string.info_button_main_content_p3), getString(R.string.info_button_main_content_p4)));
                 break;
             case"COIN":
-                // TODO: 11/04/2025 add text for 'dice roll' info page
+                ((TextView)dialog.findViewById(R.id.infoTitle)).setText(R.string.info_button_coin_flipper_title);
+                ((TextView)dialog.findViewById(R.id.infoContent)).setText("\uD83E\uDE99 How to Use the Coin Flipper:\n \n" +
+                        "1) Just give the “Flip” button a tap to send it flying.\n" +
+                        "\n" +
+                        "2) Watch it Spin - The buttons will be disabled until the coin is done.\n \n" +
+                        "3) See the Result – The coin will land on either Heads or Tails. That’s your answer!\n \n" +
+                        "You can flip as many times as you like — just don’t blame us if the coin chooses something you didn’t want \uD83D\uDE09\n \n" +
+                        "─────────────────\n" +
+                        "No coin in your pocket? No problem. This digital flipper is perfect for:\n" +
+                        "\n" +
+                        "\uD83D\uDD04 Deciding who goes first in board games like Monopoly, Risk, or Catan\n" +
+                        "\n" +
+                        "\uD83C\uDFC8 Kicking off a friendly football or soccer match\n" +
+                        "\n" +
+                        "\uD83C\uDFA4 Choosing the first speaker in a group presentation or debate\n" +
+                        "\n" +
+                        "\uD83C\uDF54 Picking where to eat when no one wants to choose\n" +
+                        "\n" +
+                        "\uD83C\uDFAE Breaking ties or choosing teams in party or video games\n" +
+                        "\n" +
+                        "☕ Settling everyday standoffs (like who’s making the coffee run)\n" +
+                        "\n");
+
                 break;
             case "DICE":
                 // TODO: 11/04/2025 add text for 'dice roll' info page
