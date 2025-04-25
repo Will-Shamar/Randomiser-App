f0f091ea0d0db7f21880c2ba966a4d5ff1be0416
WI91 <elammar.will@gmail.com>
2025-04-25 16:59:22 +0100

    Feat: (Test) Added todo to check commit functionality after file location change

--- app/src/main/java/com/example/randomiser_app/coinflipper/CoinFlipperClickHandler.java
+++ app/src/main/java/com/example/randomiser_app/coinflipper/CoinFlipperClickHandler.java
@@ -166,6 +166,7 @@ public class CoinFlipperClickHandler extends Handler implements RecyclerViewInte
             popupWindow.dismiss();
             return true;
         });
+        // TODO: 25/04/2025 Add functionality to skins menu close button 
     }
     @Override
     public void onItemClick(int position) {
